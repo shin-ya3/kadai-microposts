ca6b5a8b9cf49222b9c516d51ed20ef09d29ce56
jon <jonnojimublog@gmail.com>
2018-09-27 12:48:31 +0000

    follow, unfollow

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     @if (Auth::check())
         <?php $user = Auth::user(); ?>
-        {{ $user->name }}
+        {{ link_to_route('tasks.index') }}
     @else
         <div class="center jumbotron">
             <div class="text-center">
